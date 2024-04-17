f1ad06f15123987716601d55dc19806dc1958b1c
xueliang <btainlee@gmail.com>
2024-04-17 10:49:28 +0800

    add tailwindcss and elementui

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import store from './store'
 // 配置 axios
 import http from "@/plugins/http";
 Vue.use(http)
+// 配置 tailwindcss
+import 'tailwindcss/dist/tailwind.css'
+// 配置 elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="container">
+  <div class="container snap-center">
     <h3>HomePage</h3>
     <h4>vue2-ready-to-code</h4>
-    <img src="https://avatars.githubusercontent.com/u/56247729" alt="">
-    <textarea v-model="text"></textarea>
-    <button v-on:click="requestHello">send</button>
+    <img src="https://avatars.githubusercontent.com/u/56247729" alt="" style="max-width: 200px">
+    <div class="bg-gray-100 p-4">
+      <h1 class="text-2xl font-bold">Welcome to my website</h1>
+      <p class="mt-4">This is a paragraph decorated with tailwindcss.</p>
+    </div>
+    <el-button type="primary" v-on:click="requestHello">Click me!</el-button>
   </div>
 </template>
 
